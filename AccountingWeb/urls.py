1ed1733677b9379a1bed85db49f79e5a3f395831
shohjaxon7 <shohjaxonmamajonov777@gmail.com>
2025-03-23 17:39:38 -0700

    first commit

--- AccountingWeb/urls.py
+++ AccountingWeb/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
